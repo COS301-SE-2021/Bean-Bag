d14a79a0ea934d285d0ae084d9c4e2a42ecfb82f
nadachra <70055041+nadachra@users.noreply.github.com>
2021-06-01 21:00:45 +0200

    Landing Page and Home Page structure

---
+++ BeanBag/Controllers/LandingPageController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeanBag.Controllers
+{
+    public class LandingPageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
