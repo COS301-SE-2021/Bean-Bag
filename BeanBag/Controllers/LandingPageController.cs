dc0eee233e84cf23d906c0503c1bb844f81cc026
Munashe <u18030948@tuks.co.za>
2021-06-02 20:36:04 +0200

    Code for the QR code

--- BeanBag/Controllers/LandingPageController.cs
+++ BeanBag/Controllers/LandingPageController.cs
@@ -27,6 +27,27 @@ namespace BeanBag.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(string inputText)    //Index page but specified with the string input
+        {
+            using (MemoryStream ms = new MemoryStream())        //memory stream helps write from and to the 'memory'
+            {
+                QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();    //newQRCode generator Object instance created from package
+                QRCodeData qRCodeData = qRCodeGenerator.CreateQrCode("urlfromthedatabase", QRCodeGenerator.ECCLevel.Q);
+                //Data and input text is used to generate the QR Code's actual 'DATA' 
+                QRCode qRCode = new QRCode(qRCodeData); //make a new instance of the QR code compiled data of text and string input
+                using (Bitmap bitmap= qRCode.GetGraphic(20)) 
+                {
+                    bitmap.Save(ms, ImageFormat.Png);   //bitmap maps our string and data to image bits which are then 'drawn'
+                    ViewBag.QRCode = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());  
+                    //viewbag is connected to front end, and we convert the bitmap back to a value our front end can comprehend, an array of bits?
+                }
+
+
+            }
+            return View();  //view is returned on the front end for the entire instance of the project ;)
+        }
+
         public IActionResult Signup()
         {
             return View();
