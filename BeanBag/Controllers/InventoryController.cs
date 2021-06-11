df3b0778a97bb42b2adedb064da92961419171c4
Reno <renodavids8@gmail.com>
2021-06-11 21:07:15 +0200

    Inventory Skeleton Code

---
+++ BeanBag/Controllers/InventoryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeanBag.Controllers
+{
+    // This class is used to handle any user interaction regarding an inventory
+    public class InventoryController : Controller
+    {
+        // This is the default view to view all of the inventories associated with a user
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
