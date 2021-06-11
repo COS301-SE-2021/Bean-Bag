a18eed8826f0920e44250a4821686f585e8e9a3e
Munashe <u18030948@tuks.co.za>
2021-06-11 09:59:39 +0200

    Fixing comment conventions with periods and other stuff

--- BeanBag/Models/QRCodeModel.cs
+++ BeanBag/Models/QRCodeModel.cs
@@ -3,9 +3,10 @@
     /* This class is used to get and set all the variables related to the QRCode*/
     public class QrCodeModel
     {
+        // The unique identifier for every QR code. This QR number is used in coupling
+        // the QR code to the Item through coupling with itemID as the primary key and QRNumber as the foreign key
+        // in the item table.
         public string QrCodeNumber { get; set; }
-        //the unique identifier for every QR code. This QR number is used in coupling
-        //the QR code to the Item through coupling with itemID as the primary key and QRNumber as the foreign key
-        //in the item table
+
     }
 }
