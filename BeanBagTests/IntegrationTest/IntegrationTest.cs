a030bccc853998c9a26e7dac32ffb34ac93f3bfa
Reno <56050393+Dreddy97@users.noreply.github.com>
2021-06-17 22:15:49 +0200

    Delete BeanBagTests directory

--- BeanBagTests/IntegrationTest/IntegrationTest.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace BeanBagUnitTesting.IntegrationTest
-{
-      /* This class creates an integration test for the BeanBag system */
-      public class IntegrationTest
-      {
-          
-      }
-}
