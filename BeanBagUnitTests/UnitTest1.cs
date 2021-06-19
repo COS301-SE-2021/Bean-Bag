906bc6ff633af74398feaad3eec3b7269a60b396
Reno <56050393+Dreddy97@users.noreply.github.com>
2021-06-19 12:10:45 +0200

    Fix: Added missing BeanBagunitTests directory

---
+++ BeanBagUnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace BeanBagUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
