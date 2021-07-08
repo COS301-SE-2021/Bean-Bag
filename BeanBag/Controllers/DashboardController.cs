23ffc13a2de403c42df9afb68cb61c7d3fa1c36a
nadachra <70055041+nadachra@users.noreply.github.com>
2021-07-08 17:22:10 +0200

    Created dashboard controller to retrieve backend code

---
+++ BeanBag/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeanBag.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
