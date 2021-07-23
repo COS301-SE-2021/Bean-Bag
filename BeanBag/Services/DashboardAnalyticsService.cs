3e02200b115238224defa121d57594fb0db45575
nadachra <70055041+nadachra@users.noreply.github.com>
2021-07-23 21:06:45 +0200

    added db context to dashboard analytics service

--- BeanBag/Services/DashboardAnalyticsService.cs
+++ BeanBag/Services/DashboardAnalyticsService.cs
@@ -1,7 +1,14 @@
-﻿namespace BeanBag.Services
+﻿using BeanBag.Database;
+
+namespace BeanBag.Services
 {
     public class DashboardAnalyticsService
     {
-        
+        private readonly DBContext _db;
+
+        public DashboardAnalyticsService(DBContext db)
+        {
+            _db = db;
+        }
     }
 }
