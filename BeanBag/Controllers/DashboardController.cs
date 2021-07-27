e82a4a5e36f3b69b0db3dbfd5a52c30151c586e5
nadachra <70055041+nadachra@users.noreply.github.com>
2021-07-27 02:05:27 +0200

    created a function in dashboard controller to get the items

--- BeanBag/Controllers/DashboardController.cs
+++ BeanBag/Controllers/DashboardController.cs
@@ -1,5 +1,7 @@
-﻿using System.Dynamic;
+﻿using System;
+using System.Dynamic;
 using BeanBag.Database;
+using BeanBag.Models;
 using BeanBag.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Web;
@@ -9,14 +11,16 @@ namespace BeanBag.Controllers
     public class DashboardController : Controller
     {
         private readonly IInventoryService _inventoryService;
+        private readonly IItemService _itemService;
         // This variable is used to interact with the Database/DBContext class. Allows us to save, update and delete records 
         private readonly DBContext _db;
 
-        public DashboardController(DBContext db, IInventoryService _is)
+        public DashboardController(DBContext db, IInventoryService inv, IItemService item)
         {
             // Inits the db context allowing us to use CRUD operations on the inventory table
             _db = db;
-            _inventoryService = _is;
+            _inventoryService = inv;
+            _itemService = item;
         }
         
         // GET
@@ -27,7 +31,13 @@ namespace BeanBag.Controllers
             return View(dy);
         }
 
-    
+        // Get recently added items
+        public PartialViewResult GetItems(int x, Guid id)
+        {
+            dynamic dy = new ExpandoObject();
+            dy.item = _itemService.GetItems(id);
+            return PartialView("Index");
+        }
         
     }
 }
