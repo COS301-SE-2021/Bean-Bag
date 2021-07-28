13062daa3a9a7e18f7fc063910438b8f4874a641
nadachra <70055041+nadachra@users.noreply.github.com>
2021-07-28 15:30:53 +0200

    Created a GetTotalItems function in the Dashboard Service class

--- BeanBag/Services/DashboardAnalyticsService.cs
+++ BeanBag/Services/DashboardAnalyticsService.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Linq;
 using BeanBag.Database;
-
 namespace BeanBag.Services
 {
     public class DashboardAnalyticsService :IDashboardAnalyticsService
     {
         private readonly DBContext _db;
-
-        public DashboardAnalyticsService(DBContext db)
+        private readonly IInventoryService _inv;
+        
+        //Constructor
+        public DashboardAnalyticsService( IInventoryService inv, DBContext db)
         {
             _db = db;
+            _inv = inv;
         }
 
+        //Gets the recent items added to a specific inventory id in the functions parameter 
         public IOrderedQueryable GetRecentItems(string id)
         {
             var idd =  new Guid(id);
@@ -20,7 +23,25 @@ namespace BeanBag.Services
             var res= result.OrderByDescending(d => d.entryDate);
             return res;
         }
+        
+        //Gets the total items added by the user given the user id in the functions parameter 
+        public int GetTotalItems(string userId)
+        {
+            var inventories = _inv.GetInventories(userId);
 
+            var total = 0;
+            foreach (var inventory in inventories)
+            {
+                var res = (from i in _db.Items where i.inventoryId.Equals(inventory.Id) select new {i.quantity}).ToList();
+                for (int i = 0; i < res.Count; i++)
+                {
+                    total += res[i].quantity;
+                }
+            }
+            return total;
+            
+        }
+        
 
     }
 }
