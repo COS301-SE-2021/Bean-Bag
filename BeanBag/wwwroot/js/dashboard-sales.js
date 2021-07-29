1179d8b352b29ef97a03f68c5f8b4465f6bf826b
nadachra <70055041+nadachra@users.noreply.github.com>
2021-07-29 20:58:36 +0200

    Added ajax code for dashboard chart

--- BeanBag/wwwroot/js/dashboard-sales.js
+++ BeanBag/wwwroot/js/dashboard-sales.js
@@ -1,8 +1,9 @@
+//DEFAULTS
 
 $(function() {
     "use strict";
 // ============================================================== 
-    // Total Sale -Graphic
+    // Total Sale -Graphic 
     // ============================================================== 
     let ctx = document.getElementById("total-sale").getContext('2d');
     new Chart(ctx, {
@@ -29,3 +30,38 @@ $(function() {
 
     });
 });
+
+//Clicking Functions
+/*
+
+                  //Chart
+                 $(function() {
+                     "use strict";           
+                // ============================================================== 
+                // Total Items -Graphic 
+                // ============================================================== 
+                let ctx = document.getElementById("total-sale").getContext('2d');
+                new Chart(ctx, {
+                    type: 'doughnut',
+            
+                    data: {
+                        labels: [item1, item2, item3, "Other"],
+                        datasets: [{
+                            backgroundColor: [
+                                "#5969ff",
+                                "#ff407b",
+                                "#25d5f2",
+                                "#ffc750"
+                            ],
+                            data: [quantity1, quantity2, quantity3, other]
+                        }]
+                    },
+                    options: {
+                        legend: {
+                            display: false
+                        }
+                    }
+                });
+                });
+             
+ */
