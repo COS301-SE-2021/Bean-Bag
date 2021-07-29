c2b751851cdc6d950e439da679107bd63c583d9e
Reno <renodavids8@gmail.com>
2021-07-29 18:06:44 +0200

    Starting AI Model Service Implementation

---
+++ BeanBag/Controllers/AIModelController.cs
@@ -0,0 +1,41 @@
+﻿using BeanBag.Models;
+using BeanBag.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeanBag.Controllers
+{
+    public class AIModelController : Controller
+    {
+        private readonly IAIService aIService;
+
+        public AIModelController(IAIService _ai)
+        {
+            aIService = _ai;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateAIModel()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult newAIModel(CreateNewAIModel newModel)
+        {
+            // Create the project with it's new name
+            // Upload images along with tags needed with images (image count > 5). List<Tags>, IFormCollection
+
+            return Ok();
+        }
+    }
+}
