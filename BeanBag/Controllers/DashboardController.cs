69b174c0c9ae93451137d2c6e0490213ad3ae0a9
nadachra <70055041+nadachra@users.noreply.github.com>
2021-07-29 22:34:00 +0200

    Added comments to the dashboard controller

--- BeanBag/Controllers/DashboardController.cs
+++ BeanBag/Controllers/DashboardController.cs
@@ -12,14 +12,15 @@ namespace BeanBag.Controllers
         private readonly IInventoryService inventoryService;
         private readonly IDashboardAnalyticsService dashboardAnalyticsService;
 
+        //Constructor
         public DashboardController( IInventoryService inv, IDashboardAnalyticsService dash)
         {
             // Inits the db context allowing us to use CRUD operations on the inventory table
             inventoryService = inv;
             dashboardAnalyticsService = dash;
         }
-        
-        // GET
+
+        //Index page, returns drop-down-lists and the page view
         public IActionResult Index()
         {
             var inventories = inventoryService.GetInventories(User.GetObjectId());
@@ -39,20 +40,23 @@ namespace BeanBag.Controllers
           ViewBag.InventoryDropDownChart = inventoryDropDownChart;
           return View();
         }
-
+        
+        // Get recent items for recently added items card
         public JsonResult GetItems(string id)
         {
             var result = dashboardAnalyticsService.GetRecentItems(id);
             return Json(result);  
         }
 
+        //Get total items for total items card
         public int TotalItems(string id)
         {
             var result = dashboardAnalyticsService.GetTotalItems(id);
             return result;
             
         }
-
+        
+        //Get top 3 items for the chart in the total items card
         public JsonResult TopItems(string id , int total)
         {
             var result = dashboardAnalyticsService.GetTopItems(id);
