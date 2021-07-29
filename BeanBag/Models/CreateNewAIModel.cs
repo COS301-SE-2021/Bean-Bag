1a6de4ea2fa628f962f68b1501c6df5c6550ae0c
Reno <renodavids8@gmail.com>
2021-07-29 18:06:44 +0200

    Starting AI Model Service Implementation

---
+++ BeanBag/Models/CreateNewAIModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeanBag.Models
+{
+    public class CreateNewAIModel
+    {
+        public string name { get; set; }
+
+        public IFormFile image { get; set; }
+    }
+}
