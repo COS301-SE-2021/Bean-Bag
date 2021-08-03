bb80b823494865bb35f491f382a7315eefa9b00e
nadachra <u18113312@tuks.co.za>
2021-08-03 17:08:24 +0200

    Completed get revenue dashboard function

--- BeanBag/Services/DashboardAnalyticsService.cs
+++ BeanBag/Services/DashboardAnalyticsService.cs
@@ -63,15 +63,19 @@ namespace BeanBag.Services
 
            switch (time)
            {
-               case "Y":
+               //Year
+               case "Y": 
                    currentDate = DateTime.UtcNow.Date.AddYears(-1);
                    break;
+               //Month
                case "M":
                    currentDate = DateTime.UtcNow.Date.AddMonths(-1);
                    break;
+               //Week
                case "W":
                    currentDate = DateTime.UtcNow.Date.AddDays(-7);
                    break;
+               //Day
                case "D":
                    currentDate = DateTime.UtcNow.Date;
                    break;
@@ -94,40 +98,88 @@ namespace BeanBag.Services
 
        public int GetItemsSold(string id, string time)
        {
+           Guid newId = new Guid(id);
            int sum = 0;
-         /*  var inv = inventoryService.GetInventories(id);
-           int sum = 0;
-           foreach (var x in inv)
-           {
-              // var items = itemService.GetItems(x.Id);
 
-               foreach (var y in items)
+           DateTime currentDate;
+
+           switch (time)
+           {
+               //Year
+               case "Y": 
+                   currentDate = DateTime.UtcNow.Date.AddYears(-1);
+                   break;
+               //Month
+               case "M":
+                   currentDate = DateTime.UtcNow.Date.AddMonths(-1);
+                   break;
+               //Week
+               case "W":
+                   currentDate = DateTime.UtcNow.Date.AddDays(-7);
+                   break;
+               //Day
+               case "D":
+                   currentDate = DateTime.UtcNow.Date;
+                   break;
+               default:
+                   throw new  Exception("Invalid timespan given as input, expecting Y, M, W or D") ;
+           }
+           
+           var contents = from cnt in db.Items
+               where cnt.inventoryId == newId & cnt.entryDate >= currentDate
+               select new { cnt.quantity , cnt.isSold};
+
+           foreach (var y in contents)
+           {
+               if (y.isSold)
                {
-                   if (y.isSold)
-                   {
-                       sum += 1;
-                   }
+                   sum += y.quantity;
                }
-           }*/
+           }
            return sum;
        }
 
        //Get revenue for all inventories 
        public double GetRevenue(string id, string time)
        { 
+           Guid newId = new Guid(id);
            double sum = 0;
-          /* foreach (var x in inv)
-           {
-               var items = itemService.GetItems(x.Id);
 
-               foreach (var y in items)
+           DateTime currentDate;
+
+           switch (time)
+           {
+               //Year
+               case "Y": 
+                   currentDate = DateTime.UtcNow.Date.AddYears(-1);
+                   break;
+               //Month
+               case "M":
+                   currentDate = DateTime.UtcNow.Date.AddMonths(-1);
+                   break;
+               //Week
+               case "W":
+                   currentDate = DateTime.UtcNow.Date.AddDays(-7);
+                   break;
+               //Day
+               case "D":
+                   currentDate = DateTime.UtcNow.Date;
+                   break;
+               default:
+                   throw new  Exception("Invalid timespan given as input, expecting Y, M, W or D") ;
+           }
+           
+           var contents = from cnt in db.Items
+               where cnt.inventoryId == newId & cnt.entryDate >= currentDate
+               select new { cnt.price , cnt.isSold};
+
+           foreach (var y in contents)
+           {
+               if (y.isSold)
                {
-                   if (y.isSold)
-                   {
-                       sum += y.price;
-                   }
+                   sum += y.price;
                }
-           }*/
+           }
            return sum;
        }
        
