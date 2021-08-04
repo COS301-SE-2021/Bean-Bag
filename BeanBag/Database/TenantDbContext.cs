c4fe24390c4dc342294dd1f84fbac711dc57a77e
SuzelAlberts <73718151+SuzelAlberts@users.noreply.github.com>
2021-08-04 23:40:55 +0200

    Added Tenant database context class

---
+++ BeanBag/Database/TenantDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BeanBag.Models;
+
+namespace BeanBag.Database
+{
+    public class TenantDbContext : DbContext
+    {
+        // constructor
+        public TenantDbContext(DbContextOptions<TenantDbContext> contextOptions) : base(contextOptions)
+        {
+            
+        }
+        
+        // Entities
+        // Current tenant information
+        public DbSet<Tenants> Tenant { get; set; }
+        
+        // Current user 
+        public DbSet<Users> User { get; set; }
+        
+    }
+}
