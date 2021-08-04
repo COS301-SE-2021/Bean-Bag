8f89ad9e4847e8d49238a4054e4892985c2f707b
sphesihlemtwa <73141064+sphesihlemtwa@users.noreply.github.com>
2021-08-04 17:36:06 +0200

    Created an appearance controller and index page

---
+++ BeanBag/Controllers/AppearanceController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace BeanBag.Controllers
+{
+    public class AppearanceController : Controller
+    {
+        // This function sends a response to the Home Index page.
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
