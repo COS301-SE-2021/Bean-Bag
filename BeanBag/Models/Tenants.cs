7c2ad99dc3478ed6d18359a4fd3cff9f88977687
SuzelAlberts <73718151+SuzelAlberts@users.noreply.github.com>
2021-08-04 23:43:29 +0200

    Added tenant database model classes

---
+++ BeanBag/Models/Tenants.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeanBag.Models
+{
+    public class Tenants
+    {
+        [Key]
+        public string TenantId { get; set; }
+        
+        [Required]
+        public string TenantName { get; set; }
+    }
+}
