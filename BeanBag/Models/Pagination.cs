8186ad5bdefdc27b456acbb7350cd09fdff86aeb
nadachra <u18113312@tuks.co.za>
2021-08-05 22:57:03 +0200

    Created a pagination view model to get the inventories and display them on different pages

---
+++ BeanBag/Models/Pagination.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using X.PagedList;
+
+namespace BeanBag.Models
+{
+    //This class is used to return a pagination model of the database inventory items
+    public class Pagination
+    {
+        public Inventory Inventory { get; set; }
+        public IPagedList<Inventory> PagedList{ get; set; }
+
+    }
+    
+}
