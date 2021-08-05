7e343d8e2f13e00933f9b519bb270e991e8f829b
SuzelAlberts <73718151+SuzelAlberts@users.noreply.github.com>
2021-08-05 22:20:12 +0200

    Tenant service and interface implementation

---
+++ BeanBag/Services/ITenantService.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+namespace BeanBag.Services
+{
+    public interface ITenantService
+    {
+        public string GetTenantName(string tenantId);
+
+        public string GetUserTenant(string userId);
+    }
+}
---
+++ BeanBag/Services/TenantService.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using BeanBag.Database;
+
+namespace BeanBag.Services
+{
+    public class TenantService : ITenantService
+    {
+        private readonly TenantDbContext _tenantDb;
+        
+        public TenantService(TenantDbContext context)
+        {
+            _tenantDb = context;
+        }
+
+        //Get tenant that the user belongs to
+        public string GetUserTenant(string tenantId)
+        {
+            if (tenantId == null)
+            {
+                
+            }
+            
+            // search user in db
+            if (_tenantDb.TenantUser.Find(tenantId) == null)
+            {
+                
+            }
+            
+            // user is found in db - get result
+            var tenant = (from item
+                    in _tenantDb.TenantUser
+                where item.UserObjectId.Equals(tenantId)
+                select item.UserTenantId).Single();
+
+            return GetTenantName(tenant);
+        }
+        
+        public string GetTenantName(string userTenantId)
+        {
+            var tenantName = (from item
+                    in _tenantDb.Tenant
+                where item.TenantId.Equals(userTenantId)
+                select item.TenantName).Single();
+
+            return tenantName;
+        }
+    }
+}
