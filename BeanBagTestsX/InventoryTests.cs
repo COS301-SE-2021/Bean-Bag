0e4c98771f53b84c77dc5aad5b0191b1661b2dea
Munashe <u18030948@tuks.co.za>
2021-08-06 14:31:49 +0200

    unit test 1 with moq for DB

--- BeanBagTestsX/InventoryTests.cs
+++ BeanBagTestsX/InventoryTests.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using BeanBag.Database;
 using BeanBag.Models;
 using BeanBag.Services;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Xunit;
 
@@ -10,32 +12,54 @@ namespace BeanBagTestsX
 {
     public class InventoryTests
     {
-        private readonly InventoryService _iut;
-        private readonly Mock<DBContext> _invMock = new Mock<DBContext>();
-        
-        public InventoryTests()
-        {
-            _iut = new InventoryService(_invMock.Object);
-            
-        }
-        
-        
+        //private readonly InventoryService _iut;
+        //private readonly Mock<DBContext> _invmock = new Mock<DBContext>();
+
+
+        //public InventoryTests()
+        //{
+        //    _iut = new InventoryService(_invmock.Object);
+
+        //}
+
         [Fact]
         public void Get_user_inventories_with_valid_id()
         {
             //ARRANGE
-            string theId = "1234";
-            _invMock.Setup(x => x.Find(theId)).Returns(_iut.GetInventories(theId));
+            Guid theId1 = new();
+            Guid theId2 = new();
 
+            string u1 = "xxx";
+            string u2 = "yyy";
+
+            var data = new List<Inventory>
+            {
+                new Inventory { Id = theId1, name = "Mums 1", userId = u1},
+                new Inventory { Id = theId1, name = "Mums 1.2", userId = u1},
+                new Inventory { Id = theId2, name = "Mums 2", userId = u2},
+
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Inventory>>();
+            mockSet.As<IQueryable<Inventory>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<Inventory>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Inventory>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Inventory>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<DBContext>();
+            mockContext.Setup(c => c.Inventories).Returns(mockSet.Object);
 
             //ACT
-            var invs = _iut.GetInventories(theId);
+
+            var service = new InventoryService(mockContext.Object);
+            //var service = _iut;
+            var invs = service.GetInventories(u1);
+
 
             //ASSERT
-            Assert.Equal(new List<Inventory>(),invs );
+            Assert.Equal(2 , invs.Count);
 
         }
     }
 }
 
-//var exception = Assert.Throws<Exception>(Act);
