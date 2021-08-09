642b66df32d19f26663d8c739332bb2fb3512447
Munashe <u18030948@tuks.co.za>
2021-08-09 13:44:11 +0200

    Skeletons for all unit tests of inventory

--- BeanBagTestsX/ItemTests.cs
+++ BeanBagTestsX/ItemTests.cs
@@ -32,6 +32,103 @@ namespace BeanBagTestsX
 
         }
 
+        //Unit test defined for creating an item
+        [Fact]
+        public void Create_item()
+        {
+            //ARRANGE
+            var mockIn = new Mock<IItemService>();
+
+
+            //ACT
+
+
+            //ASSERT
+
+
+        }
+
+        //Unit test defined for deleting an item 
+        [Fact]
+        public void Delete_item()
+        {
+            //ARRANGE
+            var mockIn = new Mock<IItemService>();
+
+
+            //ACT
+
+
+            //ASSERT
+
+
+        }
+
+        //Unit test defined for editing an item 
+        [Fact]
+        public void Edit_item()
+        {
+            //ARRANGE
+            var mockIn = new Mock<IItemService>();
+
+
+            //ACT
+
+
+            //ASSERT
+
+
+        }
+
+        //Unit test defined to find an item from the database
+        [Fact]
+        public void Find_item()
+        {
+            //ARRANGE
+            var mockIn = new Mock<IItemService>();
+
+
+            //ACT
+
+
+            //ASSERT
+
+
+        }
+
+        //Unit test defined to retrieve a list of items from a database, all in the same inventory
+        [Fact]
+        public void Get_items()
+        {
+            //ARRANGE
+            var mockIn = new Mock<IItemService>();
+
+
+            //ACT
+
+
+            //ASSERT
+
+
+        }
+
+        //Unit test defined to get the inventory ID from the item passed in
+        [Fact]
+        public void Get_invID_from_item()
+        {
+            //ARRANGE
+            var mockIn = new Mock<IItemService>();
+
+
+            //ACT
+
+
+            //ASSERT
+
+
+        }
+
+
     }
     
 
