a2fdbfba0287c8193ed0e7006cef4dab6419834c
Munashe <u18030948@tuks.co.za>
2021-08-09 13:39:12 +0200

    item tests update 1

--- BeanBagTestsX/ItemTests.cs
+++ BeanBagTestsX/ItemTests.cs
@@ -16,43 +16,19 @@ namespace BeanBagTestsX
     public class ItemTests
     {
 
-        // Unit test defined for the get user inventories, a valid ID will always be passed in so no need for negative testing
+        // Unit test defined for adding a QR code to an item using the azure function
         [Fact]
-        public void Get_user_inventories_with_valid_id()
+        public void Add_QRitem_with_valid_item()
         {
             //ARRANGE
-            Guid theId1 = new("00000000-0000-0000-0000-000000000001");
-            Guid theId2 = new("00000000-0000-0000-0000-000000000002");
+            var mockIn = new Mock<IItemService>();
 
-            string u1 = "xxx";
-            string u2 = "yyy";
-
-            var mockIn = new Mock<IInventoryService>();
-
-            var data = new List<Inventory>
-            {
-                new Inventory { Id = theId1, name = "Mums 1", userId = u1},
-                new Inventory { Id = theId1, name = "Mums 1.2", userId = u1},
-
-            }.AsQueryable();
-
-            var mockSet = new Mock<DbSet<Inventory>>();
-            mockSet.As<IQueryable<Inventory>>().Setup(m => m.Provider).Returns(data.Provider);
-            mockSet.As<IQueryable<Inventory>>().Setup(m => m.Expression).Returns(data.Expression);
-            mockSet.As<IQueryable<Inventory>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            mockSet.As<IQueryable<Inventory>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
-
-            Mock<IInventoryService> myser = new Mock<IInventoryService>();
 
             //ACT
 
-            myser.Setup(x => x.GetInventories(u1)).Returns(mockSet.Object.ToList());
-
-            var tinvs = myser.Object.GetInventories(u1);
-
 
             //ASSERT
-            Assert.Equal(2, tinvs.Count);
+
 
         }
 
