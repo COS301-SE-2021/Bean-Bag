abd9e3c4c0982772c40f652e9115a90e97f8f10f
Munashe <u18030948@tuks.co.za>
2021-08-10 13:15:38 +0200

    added AITests.cs and DashboardTests.cs file skeletons

---
+++ BeanBagTestsX/AITests.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BeanBag.Database;
+using BeanBag.Models;
+using BeanBag.Services;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace BeanBagTestsX
+{
+
+
+
+    public class AiTests
+    {
+
+        // Unit test defined for the get user inventories, a valid ID will always be passed in so no need for negative testing
+        [Fact]
+        public void test_predict()
+        {
+           //ARRANGE
+           
+           
+           //ACT
+           
+           
+           //ASSERT
+
+        }
+    }
+
+}
+
+
---
+++ BeanBagTestsX/DashboardTests.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BeanBag.Database;
+using BeanBag.Models;
+using BeanBag.Services;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace BeanBagTestsX
+{
+    
+
+
+    public class DashboardTests
+    {
+
+        [Fact]
+        public void Get_recent_items_test()
+        {
+        
+
+        }
+        
+        [Fact]
+        public void Get_total_items_test()
+        {
+
+
+        }
+        
+        [Fact]
+        public void Get_top_items_test()
+        {
+
+
+        }
+        
+        [Fact]
+        public void Get_items_available_test()
+        {
+
+
+        }
+        
+        [Fact]
+        public void Get_items_sold_test()
+        {
+
+
+        }
+        
+        [Fact]
+        public void Get_revenue_test()
+        {
+
+
+        }
+        
+        [Fact]
+        public void Get_sales_growth_test()
+        {
+
+
+        }
+        
+        [Fact]
+        public void Items_revenue_stat_test()
+        {
+
+
+        }
+        
+        [Fact]
+        public void Items_sold_stat_test()
+        {
+
+
+        }
+        
+        [Fact]
+        public void Item_available_stat_test()
+        {
+
+
+        }
+        
+        
+
+    }
+    
+
+}
+
+
