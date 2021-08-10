82c63b019de042781e5b72e86acb2371f3f91e5e
Munashe <u18030948@tuks.co.za>
2021-08-10 13:20:15 +0200

    DashboardTests.cs skeletons updated

--- BeanBagTestsX/DashboardTests.cs
+++ BeanBagTestsX/DashboardTests.cs
@@ -19,6 +19,11 @@ namespace BeanBagTestsX
         [Fact]
         public void Get_recent_items_test()
         {
+        //ARRANGE
+        
+        //ACT
+        
+        //ASSERT
         
 
         }
@@ -26,7 +31,12 @@ namespace BeanBagTestsX
         [Fact]
         public void Get_total_items_test()
         {
-
+        //ARRANGE
+    
+        //ACT
+    
+        //ASSERT
+            
 
         }
         
@@ -34,54 +44,94 @@ namespace BeanBagTestsX
         public void Get_top_items_test()
         {
 
+        //ARRANGE
+    
+        //ACT
+    
+        //ASSERT
+            
 
         }
         
         [Fact]
         public void Get_items_available_test()
         {
-
-
+        //ARRANGE
+    
+        //ACT
+    
+        //ASSERT
+            
+            
         }
         
         [Fact]
         public void Get_items_sold_test()
         {
-
+        //ARRANGE
+    
+        //ACT
+    
+        //ASSERT
+            
 
         }
         
         [Fact]
         public void Get_revenue_test()
         {
-
+        //ARRANGE
+    
+        //ACT
+    
+        //ASSERT
 
         }
         
         [Fact]
         public void Get_sales_growth_test()
         {
-
+        //ARRANGE
+    
+        //ACT
+    
+        //ASSERT
+        
 
         }
         
         [Fact]
         public void Items_revenue_stat_test()
         {
-
+        //ARRANGE
+    
+        //ACT
+    
+        //ASSERT
 
         }
         
         [Fact]
         public void Items_sold_stat_test()
-        {
-
-
+        { 
+            
+        //ARRANGE
+    
+        //ACT
+    
+        //ASSERT
+            
         }
         
         [Fact]
         public void Item_available_stat_test()
         {
+
+        //ARRANGE
+    
+        //ACT
+    
+        //ASSERT
 
 
         }
