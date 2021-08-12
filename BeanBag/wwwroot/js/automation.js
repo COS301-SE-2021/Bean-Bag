e1dbedbff3e4cbd335c67b6a920093f9821c4112
SuzelAlberts <73718151+SuzelAlberts@users.noreply.github.com>
2021-08-12 02:57:09 +0200

    Added js file to control user sign up and login

---
+++ BeanBag/wwwroot/js/automation.js
@@ -0,0 +1,13 @@
+﻿// Hide sign up | sign in option if tenant is not selected
+
+let element = document.getElementById("signupsignin");
+element.style.display = 'none';
+
+function tenantSelected(selected){
+    
+    if(selected && element.style.display === 'none'){
+        element.style.display = 'block';
+    }else{
+        element.style.display = 'none';
+    }
+}
