ceac9a17c5898cfa36c03f7a360e8d6a43dc2c4f
SuzelAlberts <73718151+SuzelAlberts@users.noreply.github.com>
2021-08-13 13:36:31 +0200

    Account controller added and set up for tenant automation

---
+++ BeanBag/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeanBag.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
