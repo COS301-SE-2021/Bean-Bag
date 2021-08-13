e0e83a343c15cade823289e986b68c012173eb47
Munashe <u18030948@tuks.co.za>
2021-08-13 15:15:50 +0200

    AI and dashboard skeletons re-added (unit tests)

---
+++ BeanBagTestsX/AIServiceTests.cs
@@ -0,0 +1,21 @@
+using Xunit;
+
+namespace BeanBagUnitTests
+{
+    public class AIServiceTests
+    {
+        [Fact]
+        public void predict_tags_valid()
+        {
+            //ARRANGE
+            
+            
+            //ACT
+            
+            
+            //ASSERT
+            
+            
+        }
+    }
+}
