fe680828811bc3471a6620a730a6d32e069a4820
SuzelAlberts <73718151+SuzelAlberts@users.noreply.github.com>
2021-08-13 13:40:04 +0200

    Tenant creation functionality moved to AccountController.cs

--- BeanBag/Controllers/AccountController.cs
+++ BeanBag/Controllers/AccountController.cs
@@ -1,14 +1,34 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BeanBag.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BeanBag.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly TenantService _tenantService;
+        
         [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
+        
+        [HttpPost]
+        // Allows user to create a new tenant
+        public IActionResult CreateTenant(string tenantName, string tenantTheme)
+        {
+            if (tenantName == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                _tenantService.CreateNewTenant(tenantName);
+            }
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
