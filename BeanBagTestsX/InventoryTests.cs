3acd8174439e9587cbd3a227d6513b2090ff1f71
Munashe <u18030948@tuks.co.za>
2021-08-13 11:10:16 +0200

    updated unit tests

--- BeanBagTestsX/InventoryTests.cs
+++ BeanBagTestsX/InventoryTests.cs
@@ -93,7 +93,7 @@ namespace BeanBagUnitTests
         }
 
 
-        /*
+        
         //Unit test for deleting an inventory. It will always be valid because a guid and user id will be passed in automatically 
         [Fact]
         public void Deleting_An_Inventory()
@@ -132,16 +132,57 @@ namespace BeanBagUnitTests
             
             var updInvs = inv.GetInventories(u1);
             
-           
+            var x = updInvs.Count;
+
 
             //ASSERT
          //   Assert.Equal(1, x);
          
-         var x = updInvs.Count;
          Assert.Equal(1,x);
          Assert.True(deleted);
         }
-        */
+        
+        
+        [Fact]
+        public void Editing_An_Inventory()
+        {
+            //ARRANGE
+            Guid theId1 = new("00000000-0000-0000-0000-000000000001");
+            Guid theId2 = new("00000000-0000-0000-0000-000000000002");
+
+
+            string u1 = "123";
+
+
+            //var mockIn = new Mock<IInventoryService>();
+
+            var data = new List<Inventory>
+            {
+                new Inventory { Id = theId1, name = "testinv 1", userId = u1},
+                new Inventory { Id = theId2, name = "testinv 2", userId = u1},
+
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Inventory>>();
+            mockSet.As<IQueryable<Inventory>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<Inventory>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Inventory>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Inventory>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+          
+            //ACT
+            var context = new Mock<DBContext>();
+            context.Setup(c => c.Inventories).Returns(mockSet.Object);
+
+            InventoryService inv = new InventoryService(context.Object);
+            
+            var updInvs = inv.GetInventories(u1);
+            var isEdited = inv.EditInventory(u1, updInvs[0]);
+
+
+            //ASSERT
+            Assert.True(isEdited);
+        }
 
     }
 
