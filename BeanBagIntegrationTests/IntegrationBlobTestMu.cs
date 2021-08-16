75c2d5ecc955e5dbfdbf2bd2f8f806d04e0fda6b
Munashe <u18030948@tuks.co.za>
2021-08-16 15:06:51 +0200

    blob double test to sort

--- BeanBagIntegrationTests/IntegrationBlobTestMu.cs
+++ BeanBagIntegrationTests/IntegrationBlobTestMu.cs
@@ -10,82 +10,140 @@ using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage;
 using Xunit;
 using Microsoft.AspNetCore.Http;
+using BeanBag.Services;
+using Microsoft.Extensions.Configuration;
+using Moq;
 
 namespace BeanBagIntegrationTests
 {
-    public class IntegrationBlobTestMu
+    public class IntegrationBlobTestMu : IClassFixture<BlobStorageFixture>
     {
-        public class BlobStorageFixture : IDisposable
+
+        [Fact]
+        public async Task upload_Item_Image()
         {
-            readonly Process process;
+            //ARRANGE
 
-            public BlobStorageFixture()
+            var storageAccount = CloudStorageAccount.Parse("UseDevelopmentStorage=true");
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference("itemImages");
+
+            var myserv = new BlobStorageService();
+            
+
+            //var storageaccount = cloudstorageaccount.parse(blobstorageconfiguration.local.connectionstring);
+            //var blobclient = storageaccount.createcloudblobclient();
+            //var container = blobclient.getcontainerreference("whitelist");
+
+            var newImage = new Uri("http://" + Path.GetRandomFileName());
+           
+
+            
+
+
+            var fileMock = new Mock<IFormFile>();
+            //Setup mock file using a memory stream
+            var content = "Hello World from a Fake File";
+            var fileName = "test.pdf";
+            var ms = new MemoryStream();
+            var writer = new StreamWriter(ms);
+            writer.Write(content);
+            writer.Flush();
+            ms.Position = 0;
+            fileMock.Setup(_ => _.OpenReadStream()).Returns(ms);
+            fileMock.Setup(_ => _.FileName).Returns(fileName);
+            fileMock.Setup(_ => _.Length).Returns(ms.Length);
+
+            var sut = new BlobStorageService();
+            var file = fileMock.Object;
+
+
+            //ACT
+
+
+            await container.CreateIfNotExistsAsync();
+
+            //await myserv.uploadItemImage(File);
+
+            var result = await sut.uploadItemImage(file);
+
+
+            //ASSERT
+            using (var memoryStream = new MemoryStream())
             {
-                process = new Process
-                {
-                    StartInfo = {
-                        UseShellExecute = false,
-                        FileName = @"C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe",
-                    }
-                };
+                await container.GetBlockBlobReference("test.pdf").DownloadToStreamAsync(memoryStream);
+                var report = Encoding.UTF8.GetString(memoryStream.ToArray());
+                Assert.Contains(file.ContentType, "application/pdf");
 
-                StartAndWaitForExit("stop");
-                StartAndWaitForExit("clear all");
-                StartAndWaitForExit("start");
             }
 
-            public void Dispose()
+
+        }
+
+        [Fact]
+            public async Task upload_Test_Images()
             {
-                StartAndWaitForExit("stop");
-            }
+            //ARRANGE
+            IFormFileCollection _testImages;
+            string projId = "";
 
-            void StartAndWaitForExit(string arguments)
-            {
-                process.StartInfo.Arguments = arguments;
-                process.Start();
-                process.WaitForExit(10000);
-            }
+            //ACT
 
 
-            public void upload_Item_Image(IFormFile file)
-            {
-                //ARRANGE
-
-
-                //ACT
-
-
-                //ASSERT
-
-                
-            }
-
-            public void upload_Test_Images(IFormFileCollection testImages, string projectId)
-            {
-                //ARRANGE
-
-
-                //ACT
-
-
-                //ASSERT
+            //ASSERT
 
             }
 
-            public void delete_Test_Image_Folder(string projectId)
+            [Fact]
+            public async Task delete_Test_Image_Folder()
             {
-                //ARRANGE
+            //ARRANGE
+            string projId = "";
+
+            //ACT
 
 
-                //ACT
-
-
-                //ASSERT
+            //ASSERT
 
             }
 
 
 
         }
+
+
+    public class BlobStorageFixture : IDisposable
+    {
+        readonly Process process;
+
+        public BlobStorageFixture()
+        {
+            process = new Process
+            {
+                StartInfo = {
+                    UseShellExecute = false,
+                    FileName = @"C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe",
+                }
+            };
+
+            StartAndWaitForExit("stop");
+            StartAndWaitForExit("clear all");
+            StartAndWaitForExit("start");
+        }
+
+        public void Dispose()
+        {
+            StartAndWaitForExit("stop");
+        }
+
+        void StartAndWaitForExit(string arguments)
+        {
+            process.StartInfo.Arguments = arguments;
+            process.Start();
+            process.WaitForExit(10000);
+        }
     }
+
+
 }
+
