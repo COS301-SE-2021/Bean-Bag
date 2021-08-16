d04a4f7dbad812dba00b60a62bf93e21f4d2ed8d
SuzelAlberts <73718151+SuzelAlberts@users.noreply.github.com>
2021-08-16 13:31:09 +0200

    Added logo upload to AppearanceController.cs

--- BeanBag/Controllers/AppearanceController.cs
+++ BeanBag/Controllers/AppearanceController.cs
@@ -1,4 +1,6 @@
-﻿using BeanBag.Services;
+﻿using System.Threading.Tasks;
+using BeanBag.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Web;
 
@@ -7,10 +9,12 @@ namespace BeanBag.Controllers
     public class AppearanceController : Controller
     {
         private readonly TenantService _tenantService;
+        private readonly TenantBlobStorageService _tenantBlobStorageService;
 
-        public AppearanceController(TenantService tenantService)
+        public AppearanceController(TenantService tenantService, TenantBlobStorageService tenantBlobStorageService)
         {
             _tenantService = tenantService;
+            _tenantBlobStorageService = tenantBlobStorageService;
         }
 
         // This function sends a response to the Home Index page.
@@ -30,6 +34,19 @@ namespace BeanBag.Controllers
             _tenantService.SetTenantTheme(User.GetObjectId(), theme);
 
             return RedirectToAction("Index", "Home");
+        }
+        
+        
+        [HttpPost]
+        // Allows user to upload tenant logo
+        public async Task<IActionResult> UploadLogo([FromForm(Name = "file")] IFormFile file)
+        {
+            var image = await _tenantBlobStorageService.UploadLogoImage(file);
+
+            _tenantService.SetLogo(User.GetObjectId(),image);
+
+            return RedirectToAction("Index", "Appearance");
+
         }
     }
 }
