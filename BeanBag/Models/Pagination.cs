3293f58563751c7c87f521d5b923ce211a843531
nadachra <u18113312@tuks.co.za>
2021-08-16 17:34:23 +0200

    Created view models for ai models and ai versions

--- BeanBag/Models/Pagination.cs
+++ BeanBag/Models/Pagination.cs
@@ -1,20 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using X.PagedList;
+﻿using X.PagedList;
 
 namespace BeanBag.Models
 {
-    //This class is used to return a pagination model of the database inventory items
+    //This class is used to return a pagination model of the database inventory items and models
     public class Pagination
     {
         public Inventory Inventory { get; set; }
         
-        
         public Item Item { get; set; }
         public IPagedList<Inventory> PagedList{ get; set; }
-        
-        
-        public IPagedList<Item> PagedListItems{ get; set; }
 
+        public IPagedList<Item> PagedListItems{ get; set; }
+        
+        public IPagedList<AIModel> PagedListModels{ get; set; }
+
+        public IPagedList<AIModelVersions> PagedListVersions{ get; set; }
     }
     
 }
