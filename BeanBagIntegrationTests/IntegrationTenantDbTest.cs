a277de4784328f9c03b4086cae87ab3888941e5f
SuzelAlberts <73718151+SuzelAlberts@users.noreply.github.com>
2021-08-17 18:17:26 +0200

    Setup integration testing class for tenant database

---
+++ BeanBagIntegrationTests/IntegrationTenantDbTest.cs
@@ -0,0 +1,33 @@
+﻿using BeanBag;
+using BeanBag.Database;
+using BeanBag.Models;
+using BeanBag.Services;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BeanBagIntegrationTests
+{
+    public class IntegrationTenantDbTest
+    {
+        private TenantDbContext _tenantDbContext;
+
+        public IntegrationTenantDbTest()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkSqlServer()
+                .BuildServiceProvider();
+
+            var builder = new DbContextOptionsBuilder<TenantDbContext>();
+            builder.UseSqlServer("Server=tcp:polariscapestone.database.windows.net,1433;Initial Catalog=Bean-Bag-Tenants;Persist Security Info=False;User ID=polaris;Password=MNRSSp103;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
+                .UseInternalServiceProvider(serviceProvider);
+
+            _tenantDbContext = new TenantDbContext(builder.Options);
+        }
+    }
+}
