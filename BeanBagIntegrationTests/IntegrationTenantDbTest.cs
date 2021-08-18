01a56b07cffa731c781d4f23184ccb75959ee77e
SuzelAlberts <73718151+SuzelAlberts@users.noreply.github.com>
2021-08-18 10:55:50 +0200

    Positive testing getting list of tenants

--- BeanBagIntegrationTests/IntegrationTenantDbTest.cs
+++ BeanBagIntegrationTests/IntegrationTenantDbTest.cs
@@ -15,7 +15,7 @@ namespace BeanBagIntegrationTests
 {
     public class IntegrationTenantDbTest
     {
-        private TenantDbContext _tenantDbContext;
+        private readonly TenantDbContext _tenantDbContext;
 
         public IntegrationTenantDbTest()
         {
@@ -29,5 +29,48 @@ namespace BeanBagIntegrationTests
 
             _tenantDbContext = new TenantDbContext(builder.Options);
         }
+        
+        
+        //Test to get a list of tenants from the database
+        //POSITIVE TEST
+        [Fact]
+        public void Get_Tenants_From_Database_Success()
+        {
+            //Arrange
+            var tenantId1 = Guid.NewGuid();
+            var tenantName1 = "Tenant-1";
+            var tenant1 = new Tenant {TenantId = tenantId1.ToString(), TenantName = tenantName1};
+            
+            var tenantId2 = Guid.NewGuid();
+            var tenantName2 = "Tenant-2";
+            var tenant2 = new Tenant {TenantId = tenantId2.ToString(), TenantName = tenantName2};
+            
+            var query = new TenantService(_tenantDbContext);
+            var tenantList = query.GetTenantList();
+            
+            
+            //Act
+            var queryExisting = new TenantService(_tenantDbContext);
+            var existing = queryExisting.GetTenantList().Count();
+            
+            _tenantDbContext.Tenant.Add(tenant1);
+            _tenantDbContext.Tenant.Add(tenant2);
+            _tenantDbContext.SaveChanges();
+            
+            var tenant = _tenantDbContext.Tenant.Find(tenantId1.ToString());
+
+            
+            //Assert
+            Assert.Equal(2+existing,tenantList.Count());
+            Assert.Equal(tenantId1.ToString(),tenant.TenantId);
+            Assert.Equal("Tenant-1", tenant.TenantName);
+            
+            
+            //Delete tenants from database
+            _tenantDbContext.Remove(_tenantDbContext.Tenant.Find(tenantId1.ToString()));
+            _tenantDbContext.Remove(_tenantDbContext.Tenant.Find(tenantId2.ToString()));
+            _tenantDbContext.SaveChanges();
+        }
+        
     }
 }
