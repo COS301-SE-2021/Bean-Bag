241c39e706240699c63d97791c1cce4fc44ab4c0
nadachra <u18113312@tuks.co.za>
2021-08-24 00:21:54 +0200

    Updated comments and naming conventions for Home controller.

--- BeanBag/Controllers/HomeController.cs
+++ BeanBag/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
-﻿
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using BeanBag.Models;
 using BeanBag.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -9,22 +7,24 @@ using Microsoft.Identity.Web;
 
 namespace BeanBag.Controllers
 {
+    /* This controller is used to send and retrieve data to the dashboard
+    view using dashboard analytics service functions. */
     public  class  HomeController : Controller
     {
+        // Global variables needed for calling the service classes.
         private readonly IInventoryService inventoryService;
         private readonly IDashboardAnalyticsService dashboardAnalyticsService;
         private readonly IItemService itemService;
 
-        //Constructor
+        // Constructor.
         public HomeController(IInventoryService inv, IDashboardAnalyticsService dash, IItemService itm)
-        {
-            // Inits the db context allowing us to use CRUD operations on the inventory table
+        { 
             inventoryService = inv;
             dashboardAnalyticsService = dash;
             itemService = itm;
         }
 
-        //Index page, returns drop-down-lists and the page view
+        // This function returns the Index page for the dashboard, returns drop-down-lists for the page view.
         public IActionResult Index()
         {
             //Inventory Drop-Down-List
@@ -54,31 +54,27 @@ namespace BeanBag.Controllers
             ViewBag.TimeDropDown = times;
             ViewBag.hasItems = true;
 
-            //Checking inventories for an empty state 
+            // Checking inventories for an empty state 
             foreach (var t in inventories)
             {
-                ViewBag.wtf = itemService.GetItems(t.Id);
-                
                 if (inventories.Count==0 || itemService.GetItems(t.Id).Count ==0)
                 {
                     ViewBag.hasItems = false;
-              
                     break;
                 }
-                
             }
 
             return View();
         }
         
-        // Get recent items for recently added items card
+        // This function gets the recent items for recently added items card.
         public JsonResult GetItems(string id)
         {
             var result = dashboardAnalyticsService.GetRecentItems(id);
             return Json(result);  
         }
 
-        //Get total items for total items card
+        // This function gets the total items for total items card.
         public int TotalItems(string id)
         {
             var result = dashboardAnalyticsService.GetTotalItems(id);
@@ -86,35 +82,35 @@ namespace BeanBag.Controllers
             
         }
         
-        //Get top 3 items for the chart in the total items card
+        // This function gets the  top 3 items for the chart in the total items card.
         public JsonResult TopItems(string id , int total)
         {
             var result = dashboardAnalyticsService.GetTopItems(id);
             return Json(result);  
         }
         
-        //Get items available for items available card
+        // This function gets the  items available for items available card.
         public int ItemsAvailable(string id, string time)
         {
             var result = dashboardAnalyticsService.GetItemsAvailable(id , time); 
             return result;
         }
 
-        //Items sold for items card
+        // This function gets the items sold for items card.
         public int ItemsSold(string id, string time)
         {
             var result = dashboardAnalyticsService.GetItemsSold(id , time); 
             return result;
         }
 
-        //Revenue 
+        //This function gets the revenue over a specific time.
         public double Revenue(string id, string time)
         {
             var result = dashboardAnalyticsService.GetRevenue(id , time); 
             return result;
         }
         
-        //Growth
+        //This function gets the sales growth over a specific time.
         public double SalesGrowth(string id, string time)
         {
             var result = dashboardAnalyticsService.GetSalesGrowth(id , time); 
@@ -122,7 +118,7 @@ namespace BeanBag.Controllers
         }
         
             
-        //Item available percentage growth statistic
+        // This function gets the item available percentage growth statistic over a specific time.
         public double AvailableStat(string id, string time)
         {
             var result = dashboardAnalyticsService.ItemAvailableStat(id , time); 
@@ -130,14 +126,14 @@ namespace BeanBag.Controllers
         }
         
                
-        //Item sold percentage growth statistic
+        //This function gets the item sold percentage growth statistic over a specific time.
         public double SoldStat(string id, string time)
         {
             var result = dashboardAnalyticsService.ItemsSoldStat(id , time); 
             return result;
         }
         
-        //Item revenue percentage growth statistic
+        //This function gets the item revenue percentage growth statistic over a specific time.
         public double RevenueStat(string id, string time)
         {
             var result = dashboardAnalyticsService.ItemsRevenueStat(id , time); 
