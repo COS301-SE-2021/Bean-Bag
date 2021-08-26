ab65c8689b5994dae26a61cf064a631f8ea09a4c
nadachra <u18113312@tuks.co.za>
2021-08-27 00:56:29 +0200

    created a payment controller

---
+++ BeanBag/Controllers/PaymentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeanBag.Controllers
+{
+    public class PaymentController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
