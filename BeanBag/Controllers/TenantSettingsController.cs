5e4cd2752115324cbbae1db4b774bc09be3c4373
SuzelAlberts <73718151+SuzelAlberts@users.noreply.github.com>
2021-08-31 20:13:23 +0200

    Updated TenantSettingsController.cs

--- BeanBag/Controllers/TenantSettingsController.cs
+++ BeanBag/Controllers/TenantSettingsController.cs
@@ -1,12 +1,98 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BeanBag.Models;
+using BeanBag.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+using X.PagedList;
 
 namespace BeanBag.Controllers
 {
     public class TenantSettingsController : Controller
     {
-        public IActionResult Index()
+        
+        // Global variables needed for calling the service classes.
+        private readonly TenantService _tenantService;
+
+        // Constructor.
+        public TenantSettingsController(TenantService tenantService)
         {
-            return View();
+            _tenantService = tenantService;
         }
+
+        [AllowAnonymous]
+    public IActionResult Index(string sortOrder, string currentFilter, string searchString,
+            int? page,DateTime from, DateTime to)
+        {
+            if(User.Identity is {IsAuthenticated: true})
+            {
+                
+             //A ViewBag property provides the view with the current sort order, because this must be included in 
+             //the paging links in order to keep the sort order the same while paging
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            List<TenantUser> modelList;
+
+            //ViewBag.CurrentFilter, provides the view with the current filter string.
+            //he search string is changed when a value is entered in the text box and the submit button is pressed. In that case, the searchString parameter is not null.
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+
+            var model = from s in _tenantService.GetUserList(User.GetObjectId())
+                select s;
+                //Search and match data, if search string is not null or empty
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    model = model.Where(s => s.UserName.Contains(searchString));
+                }
+
+                var users = model.ToList();
+                switch (sortOrder)
+                {
+                    case "name_desc":
+                        modelList = users.OrderByDescending(s => s.UserName).ToList();
+                        break;
+                 
+                    default:
+                        modelList = users.OrderBy(s => s.UserName).ToList();
+                        break;
+                }
+                
+            //indicates the size of list
+            int pageSize = 5;
+            //set page to one is there is no value, ??  is called the null-coalescing operator.
+            int pageNumber = (page ?? 1);
+            //return the Model data with paged
+
+            TenantUser tenantUser = new TenantUser();
+            Pagination viewModel = new Pagination();
+            IPagedList<TenantUser> pagedList = modelList.ToPagedList(pageNumber, pageSize);
+
+            viewModel.TenantUser = tenantUser;
+            viewModel.PagedListTenantUsers = pagedList;
+           // @ViewBag.totalInventories = _inventoryService.GetInventories(User.GetObjectId()).Count;
+            
+            //Checking user role is in DB
+            //CheckUserRole();
+            return View(viewModel);
+            }
+            else
+            {
+                return LocalRedirect("/");
+            }
+        }
+        
+        
     }
 }
