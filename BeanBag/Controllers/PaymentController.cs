42591548707631fa06b34023803550e276aee924
nadachra <u18113312@tuks.co.za>
2021-09-01 21:42:11 +0200

    updated payment controller, fixed bugs concerning transactions

--- BeanBag/Controllers/PaymentController.cs
+++ BeanBag/Controllers/PaymentController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using BeanBag.Models;
 using BeanBag.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,55 +12,39 @@ namespace BeanBag.Controllers
 {
     public class PaymentController : Controller
     {
-        private readonly IPaymentService _paymentService;
-        
-        //For testing purposes, an actual merchant would require the actual Pay-gate key and ID 
-        private const string PayGateId = "10011072130";
-        private const string PayGateKey = "secret";
+          private readonly IPaymentService _payment;
+          readonly string PayGateID = "10011072130"; 
+          readonly string _payGateKey = "secret";
 
-        //Constructor
-        public PaymentController(IPaymentService paymentService)
+        public PaymentController(IPaymentService payment)
         {
-            _paymentService = paymentService;
+            _payment = payment;
         }
 
-        public async Task<JsonResult> GetRequest()
+        public async Task<IActionResult> GetRequest()
         {
             HttpClient http = new HttpClient();
             Dictionary<string, string> request = new Dictionary<string, string>();
-            
-            // Add if statement here and choose the payment type based on what the type of subscription the
-            // user chose.
-            
             string paymentAmount = (50 * 100).ToString("00"); // amount int cents e.i 50 rands is 5000 cents
 
-            request.Add("PAYGATE_ID", PayGateId);
-            request.Add("REFERENCE", "Beanbag Standard Order"); // Payment ref e.g ORDER NUMBER
-            request.Add("AMOUNT", paymentAmount);
+            request.Add("PAYGATE_ID", "10011072130");
+            request.Add("REFERENCE", "pgtest_"); // Payment ref e.g ORDER NUMBER
+            request.Add("AMOUNT", "5000");
             request.Add("CURRENCY", "ZAR"); // South Africa
-            //return URL needs to be secure, determine this url once the 
             request.Add("RETURN_URL", "https://6cdc-102-250-1-245.ngrok.io");
             request.Add("TRANSACTION_DATE", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
             request.Add("LOCALE", "en-za");
             request.Add("COUNTRY", "ZAF");
 
             // get authenticated user's email
-            // use a valid email, pay-gate will send a transaction confirmation to it
-            if(User.Identity is {IsAuthenticated: true})
-            {
-                //how to get the users email?
-               // request.Add("EMAIL", User.Identity.Name);
-            } else
-            {
+            // use a valid email, pay=gate will send a transaction confirmation to it
+          
                 // put your own email address for the payment confirmation (dev only)
-              //  request.Add("EMAIL", "chrafnadax@gmail.com");
-            }
+                request.Add("EMAIL", "chrafnadax@gmail.com");
             
-            //Using my email for testing purposes
-            request.Add("EMAIL", "chrafnadax@gmail.com");
-            request.Add("CHECKSUM", _paymentService.GetMd5Hash(request, PayGateKey));
+            request.Add("CHECKSUM", _payment.GetMd5Hash(request, _payGateKey));
 
-            string requestString = _paymentService.ToUrlEncodedString(request);
+            string requestString = _payment.ToUrlEncodedString(request);
             StringContent content = new StringContent(requestString, Encoding.UTF8, "application/x-www-form-urlencoded");
             HttpResponseMessage response = await http.PostAsync("https://secure.paygate.co.za/payweb3/initiate.trans", content);
 
@@ -68,7 +53,7 @@ namespace BeanBag.Controllers
 
             string responseContent = await response.Content.ReadAsStringAsync();
 
-            Dictionary<string, string> results = _paymentService.ToDictionary(responseContent);
+            Dictionary<string, string> results = _payment.ToDictionary(responseContent);
 
             if (results.Keys.Contains("ERROR"))
             {
@@ -76,35 +61,34 @@ namespace BeanBag.Controllers
                 {
                     success = false,
                     message = "An error occured while initiating your request"
-                },  new Newtonsoft.Json.JsonSerializerSettings());
+                });
             }
 
-            if (!_paymentService.VerifyMd5Hash(results, PayGateKey, results["CHECKSUM"]))
+            if (!_payment.VerifyMd5Hash(results, _payGateKey, results["CHECKSUM"]))
             {
                 return Json(new
                 {
                     success = false,
                     message = "MD5 verification failed"
-                },  new Newtonsoft.Json.JsonSerializerSettings());
+                });
             }
 
-        /*    bool isRecorded = _paymentService.AddTransaction(request, results["PAY_REQUEST_ID"]);
-            if (isRecorded)
+            //NEED THE DB TO ENSURE THE TRANSACTION IS SAVED 
+          //  bool IsRecorded = _payment.AddTransaction(request, results["PAY_REQUEST_ID"]);
+            if (true)
             {
                 return Json(new
                 {
                     success = true,
                     message = "Request completed successfully",
                     results
-                },  new Newtonsoft.Json.JsonSerializerSettings());
-            }*/
-            
-            
-            return Json(new
+                });
+            }
+          /*  return Json(new
             {
                 success = false,
                 message = "Failed to record a transaction"
-            },  new Newtonsoft.Json.JsonSerializerSettings());
+            });*/
         }
 
         // This is your return url from Paygate
@@ -112,65 +96,64 @@ namespace BeanBag.Controllers
         [HttpPost]
         public async Task<ActionResult> CompletePayment()
         {
-            string responseContent = Request.ToString();
-            Dictionary<string, string> results = _paymentService.ToDictionary(responseContent);
+            string responseContent = Request.RouteValues.ToString();
+            Dictionary<string, string> results = _payment.ToDictionary(responseContent);
 
-           /* Transaction transaction = _paymentService.GetTransaction(results["PAY_REQUEST_ID"]);
+            Transaction transaction = _payment.GetTransaction(results["PAY_REQUEST_ID"]);
 
             if (transaction == null)
             {
-                // Unable to reconcile transaction
-                return  RedirectToAction("Failed");
-            }*/
+                // Unable to reconsile transaction
+                return RedirectToAction("Failed");
+            }
 
             // Reorder attributes for MD5 check
-            Dictionary<string, string> validationSet = new Dictionary<string, string>
-            {
-                {"PAYGATE_ID", PayGateId},
-                {"PAY_REQUEST_ID", results["PAY_REQUEST_ID"]},
-                {"TRANSACTION_STATUS", results["TRANSACTION_STATUS"]}
-            };
-            // validationSet.Add("REFERENCE", transaction.REFERENCE);
+            Dictionary<string, string> validationSet = new Dictionary<string, string>();
+            validationSet.Add("PAYGATE_ID", PayGateID);
+            validationSet.Add("PAY_REQUEST_ID", results["PAY_REQUEST_ID"]);
+            validationSet.Add("TRANSACTION_STATUS", results["TRANSACTION_STATUS"]);
+            validationSet.Add("REFERENCE", transaction.REFERENCE);
 
-            if (!_paymentService.VerifyMd5Hash(validationSet, PayGateKey, results["CHECKSUM"]))
+            if (!_payment.VerifyMd5Hash(validationSet, _payGateKey, results["CHECKSUM"]))
             {
                 // checksum error
                 return RedirectToAction("Failed");
             }
             
-            /* Payment Status 
-              -2 = Unable to reconcile transaction
-              -1 = Checksum Error
-              0 = Pending
-              1 = Approved
-              2 = Declined
-              3 = Cancelled
-              4 = User Cancelled
+            /** Payment Status 
+             * -2 = Unable to reconsile transaction
+             * -1 = Checksum Error
+             * 0 = Pending
+             * 1 = Approved
+             * 2 = Declined
+             * 3 = Cancelled
+             * 4 = User Cancelled
              */
             
             int paymentStatus = int.Parse(results["TRANSACTION_STATUS"]);
             if(paymentStatus == 1)
             {
                 // Yey, payment approved
+                // Do something useful
             }
             // Query paygate transaction details
             // And update user transaction on your database
-          //  await VerifyTransaction(responseContent, transaction.REFERENCE);
+            await VerifyTransaction(responseContent, transaction.REFERENCE);
             return RedirectToAction("Complete", new { id = results["TRANSACTION_STATUS"] });
         }
 
-        private async Task VerifyTransaction(string responseContents, string reference)
+        private async Task VerifyTransaction(string responseContent, string Referrence)
         {
             HttpClient client = new HttpClient();
-            Dictionary<string, string> response = _paymentService.ToDictionary(responseContents);
-            Dictionary<string, string> request = new Dictionary<string, string>
-            {
-                {"PAYGATE_ID", PayGateId}, {"PAY_REQUEST_ID", response["PAY_REQUEST_ID"]}, {"REFERENCE", reference}
-            };
+            Dictionary<string, string> response = _payment.ToDictionary(responseContent);
+            Dictionary<string, string> request = new Dictionary<string, string>();
 
-            request.Add("CHECKSUM", _paymentService.GetMd5Hash(request, PayGateKey));
+            request.Add("PAYGATE_ID", PayGateID);
+            request.Add("PAY_REQUEST_ID", response["PAY_REQUEST_ID"]);
+            request.Add("REFERENCE", Referrence);
+            request.Add("CHECKSUM", _payment.GetMd5Hash(request, _payGateKey));
 
-            string requestString = _paymentService.ToUrlEncodedString(request);
+            string requestString = _payment.ToUrlEncodedString(request);
 
             StringContent content = new StringContent(requestString, Encoding.UTF8, "application/x-www-form-urlencoded");
 
@@ -178,22 +161,23 @@ namespace BeanBag.Controllers
             HttpResponseMessage res = await client.PostAsync("https://secure.paygate.co.za/payweb3/query.trans", content);
             res.EnsureSuccessStatusCode();
 
-            string responseContent = await res.Content.ReadAsStringAsync();
+            string _responseContent = await res.Content.ReadAsStringAsync();
 
-            Dictionary<string, string> results = _paymentService.ToDictionary(responseContent);
+            Dictionary<string, string> results = _payment.ToDictionary(_responseContent);
             if (!results.Keys.Contains("ERROR"))
             {
-             //   _paymentService.UpdateTransaction(results, results["PAY_REQUEST_ID"]);
+                _payment.UpdateTransaction(results, results["PAY_REQUEST_ID"]);
             }
 
         }
+
         public ViewResult Complete(int? id)
         {
             string status = "Unknown";
             switch (id.ToString())
             {
                 case "-2":
-                    status = "Unable to reconcile transaction";
+                    status = "Unable to reconsile transaction";
                     break;
                 case "-1":
                     status = "Checksum Error. The values have been altered";
@@ -226,7 +210,7 @@ namespace BeanBag.Controllers
         {
             throw new NotImplementedException();
         }
-        
+    
         public IActionResult Billing()
         {
             return View();
