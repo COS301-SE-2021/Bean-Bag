0e8340f2288847d2f802f16b07f6ef4ec2c65e34
nadachra <u18113312@tuks.co.za>
2021-09-01 14:40:35 +0200

    Created a transaction model

---
+++ BeanBag/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BeanBag.Models
+{
+    public class Transaction
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid TRANSACTION_ID { get; set; }
+        public DateTime? DATE { get; set; }
+        public string PAY_REQUEST_ID { get; set; }
+        public int AMOUNT { get; set; }
+        public string REFERENCE { get; set; }
+        public string TRANSACTION_STATUS { get; set; }
+        public string RESULT_DESC { get; set; }
+        public string CUSTOMER_EMAIL_ADDRESS { get; set; }
+        public Guid TENANT_ID { get; set; }
+        public string SUBSCRIPTION { get; set; }
+        
+    }
+}
