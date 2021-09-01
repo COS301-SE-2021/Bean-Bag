0bb4a217311f8e260d50e1617315f0c756655b2a
nadachra <u18113312@tuks.co.za>
2021-09-01 14:40:49 +0200

    Created a transaction db context

---
+++ BeanBag/Database/TransactionDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BeanBag.Models;
+
+namespace BeanBag.Database
+{
+     public class TransactionDbContext : DbContext
+        {
+            public TransactionDbContext(DbContextOptions<TransactionDbContext> contextOptions) : base(contextOptions) {}
+            public DbSet<Transaction> Transaction { get; set; }
+
+        }
+}
