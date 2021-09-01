9c79e32a96389fde153f5ac98c80f12d390c0c49
nadachra <u18113312@tuks.co.za>
2021-09-01 14:39:57 +0200

    Added transaction, security md5 and utility functions to the payment service class

--- BeanBag/Services/PaymentService.cs
+++ BeanBag/Services/PaymentService.cs
@@ -1,7 +1,178 @@
-﻿namespace BeanBag.Services
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using BeanBag.Database;
+using BeanBag.Models;
+
+namespace BeanBag.Services
 {
     public class PaymentService
     {
+      //  private readonly TenantDbContext context;
+         private TransactionDbContext _transactionDb;
+
+         //Constructor sets database context
+         public PaymentService( TransactionDbContext transactionDb)
+         {
+             _transactionDb = transactionDb;
+         }
+
+        #region Utilities
+        /** Encode dictionary to Url string */
+        public string ToUrlEncodedString(Dictionary<string, string> request)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach (string key in request.Keys)
+            {
+                builder.Append("&");
+                builder.Append(key);
+                builder.Append("=");
+                builder.Append(HttpUtility.UrlEncode(request[key]));
+            }
+
+            string result = builder.ToString().TrimStart('&');
+
+            return result;
+        }
+
+        /** Convert query string to dictionary */
+        public Dictionary<string, string> ToDictionary(string response)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            string[] valuePairs = response.Split('&');
+            foreach (string valuePair in valuePairs)
+            {
+                string[] values = valuePair.Split('=');
+                result.Add(values[0], HttpUtility.UrlDecode(values[1]));
+            }
+
+            return result;
+        }
+        #endregion Utility
+
+        #region MD5 Hashing
+        // Adapted from
+        // https://msdn.microsoft.com/en-us/library/system.security.cryptography.md5(v=vs.110).aspx
+
+        public string GetMd5Hash(Dictionary<string, string> data, string encryptionKey)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                StringBuilder input = new StringBuilder();
+                foreach (string value in data.Values)
+                {
+                    input.Append(value);
+                }
+
+                input.Append(encryptionKey);
+
+                byte[] hash = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input.ToString()));
+
+                StringBuilder sBuilder = new StringBuilder();
+
+                for (int i = 0; i < hash.Length; i++)
+                {
+                    sBuilder.Append(hash[i].ToString("x2"));
+                }
+                return sBuilder.ToString();
+            }
+        }
+
+        public bool VerifyMd5Hash(Dictionary<string, string> data, string encryptionKey, string hash)
+        {
+            Dictionary<string, string> hashDict = new Dictionary<string, string>();
+
+            foreach (string key in data.Keys)
+            {
+                if (key != "CHECKSUM")
+                {
+                    hashDict.Add(key, data[key]);
+                }
+            }
+
+            string hashOfInput = GetMd5Hash(hashDict, encryptionKey);
+
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        #endregion MD5 Hash
+
         
+        #region Transactions 
+        public bool AddTransaction(Dictionary<string, string> request, string payRequestId)
+        {
+          /*  try
+            {
+                Transaction transaction = new Transaction()
+                {
+                    DATE = DateTime.Now,
+                    PAY_REQUEST_ID = payRequestId,
+                    REFERENCE = request["REFERENCE"],
+                    AMOUNT = int.Parse(request["AMOUNT"]),
+                    CUSTOMER_EMAIL_ADDRESS = request["EMAIL"]
+                };
+                _transactionDb.Transactions.Add(transaction);
+                _transactionDb.SaveChanges();
+                return true;
+            } catch (Exception )
+            {
+                // log somewhere
+                // at least we tried
+                return false;
+            }*/
+          return false;
+        }
+        // get transaction using pay request Id
+        public Transaction GetTransaction(string payRequestId)
+        {
+           /* Transaction transaction = _transactionDb.Transactions.FirstOrDefault(p => p.PAY_REQUEST_ID == payRequestId);
+            if (transaction == null)
+            {
+                return new Transaction();
+            }
+
+            return transaction;*/
+           return null;
+        }
+
+        public bool UpdateTransaction(Dictionary<string, string> request, string PayrequestId)
+        {
+          /*  bool isUpdated = false;
+
+            Transaction transaction = GetTransaction(PayrequestId);
+            if (transaction == null)
+                return false;
+
+            transaction.TRANSACTION_STATUS = request["TRANSACTION_STATUS"];
+            transaction.RESULT_DESC = request["RESULT_DESC"];
+            try
+            {
+                _transactionDb.SaveChanges();
+                isUpdated = true;
+            } catch (Exception )
+            {
+                
+            }
+            return isUpdated;*/
+          return false;
+        }
+
+        #endregion Transaction
+
+     
+    
     }
 }
