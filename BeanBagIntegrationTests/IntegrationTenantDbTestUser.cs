e8a54a5c3272672069f6149e7a83ba166fbbb4d1
SuzelAlberts <73718151+SuzelAlberts@users.noreply.github.com>
2021-09-04 16:24:15 +0200

    Updated Guid type

--- BeanBagIntegrationTests/IntegrationTenantDbTestUser.cs
+++ BeanBagIntegrationTests/IntegrationTenantDbTestUser.cs
@@ -33,7 +33,7 @@ namespace BeanBagIntegrationTests
         {
             //Arrange
             //Tenant
-            var id = Guid.NewGuid().ToString();
+            var id = Guid.NewGuid();
             var name = "Tenant-name";
             var newTenant = new Tenant { TenantId = id, TenantName = name };
             
