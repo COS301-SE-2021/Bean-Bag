24c4991a08587a76d13c6e87c062131cca97a149
nadachra <u18113312@tuks.co.za>
2021-09-06 20:38:33 +0200

    Updated create tenant function

--- BeanBag/Services/ITenantService.cs
+++ BeanBag/Services/ITenantService.cs
@@ -12,7 +12,7 @@ namespace BeanBag.Services
         public Tenant GetCurrentTenant(string userId);
         public string GetTenantTheme(string userId);
         public bool SetTenantTheme(string userId, string theme);
-        public bool CreateNewTenant(string tenantName, string address, string email, string number);
+        public bool CreateNewTenant(string tenantName, string address, string email, string number, string subscription);
         public void EditTenantDetails(string tenantId, string tenantName, string address, string email, string number);
         public bool SearchTenant(string tenantId);
         public IEnumerable<Tenant> GetTenantList();
