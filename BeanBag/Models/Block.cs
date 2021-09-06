37ae2916741c10c3216b6470edd3d05497773873
Munashe <u18030948@tuks.co.za>
2021-09-06 19:36:26 +0200

    added block model with hash calculations

---
+++ BeanBag/Models/Block.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BeanBag.Models
+{
+    public class Block
+    {
+        public int Index { get; set; }  
+        public DateTime TimeStamp { get; set; }  
+        public string PreviousHash { get; set; }  
+        public string Hash { get; set; }  
+        public string Data { get; set; }  
+  
+        public Block(DateTime timeStamp, string previousHash, string data)  
+        {  
+            Index = 0;  
+            TimeStamp = timeStamp;  
+            PreviousHash = previousHash;  
+            Data = data;  
+            Hash = CalculateHash();  
+        }  
+  
+        public string CalculateHash()  
+        {  
+            SHA256 sha256 = SHA256.Create();  
+  
+            byte[] inputBytes = Encoding.ASCII.GetBytes($"{TimeStamp}-{PreviousHash ?? ""}-{Data}");  
+            byte[] outputBytes = sha256.ComputeHash(inputBytes);  
+  
+            return Convert.ToBase64String(outputBytes);  
+        }  
+    }
+}
