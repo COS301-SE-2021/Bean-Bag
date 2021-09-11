e46eb28f2685eadea8e3fd1d0c715ae7bbd57888
SuzelAlberts <73718151+SuzelAlberts@users.noreply.github.com>
2021-09-11 13:36:15 +0200

    Controller for user sign up added

---
+++ BeanBag/Controllers/UserSignUpController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeanBag.Controllers
+{
+    public class UserSignUpController : Controller
+    {
+
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
