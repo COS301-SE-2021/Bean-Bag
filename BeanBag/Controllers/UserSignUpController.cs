15d42cdc96593ea2c6f3732767fe74a0e6768a66
SuzelAlberts <73718151+SuzelAlberts@users.noreply.github.com>
2021-09-11 14:12:14 +0200

    Constructor for initialization and verifying user invite code

--- BeanBag/Controllers/UserSignUpController.cs
+++ BeanBag/Controllers/UserSignUpController.cs
@@ -1,15 +1,43 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using BeanBag.Services;
 
 namespace BeanBag.Controllers
 {
     public class UserSignUpController : Controller
     {
 
+        private readonly ITenantService _tenantService;
+        public UserSignUpController(ITenantService tenantService)
+        {
+            _tenantService = tenantService;
+        }
+
         [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult VerifyCode(string code)
+        {
+            if (code == null)
+            {
+                return BadRequest();
+            }
+            
+            //Get tenant id
+            var tenantId = "";
+            
+            //Verify the entered code
+            if (_tenantService.VerifyCode(tenantId,code))
+            {
+                return RedirectToAction("SignIn", "Account");
+            }
+
+            return RedirectToAction("Index");
         }
 
     }
