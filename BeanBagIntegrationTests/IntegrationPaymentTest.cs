b9e6c2b6d62743f192b6f8addb567577072160a6
Munashe <u18030948@tuks.co.za>
2021-09-14 12:24:53 +0200

    get transctions test added to be fixed

--- BeanBagIntegrationTests/IntegrationPaymentTest.cs
+++ BeanBagIntegrationTests/IntegrationPaymentTest.cs
@@ -245,11 +245,53 @@ namespace BeanBagIntegrationTests
             
         }
         
+        [Fact]
+        public void Get_Transactions_valid()
+        {
+            
+            //ARRANGE
+            var chars = "0123456789";
+            var stringChars = new char[5];
+            var random = new Random();
+
+            for (var i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+
+            var myGuidEnd = finalString;
+
+            var u2 = finalString.Substring(0, 4);
+
+            Guid theId2 = new("00000000-0000-0000-0000-0000000" + myGuidEnd);
+
+            Guid theIdPay = new("00000000-0000-0000-0000-0160000" + myGuidEnd);
+
+            string tReference = "testtransaction";
+            string tPayId = theIdPay.ToString();
+            string tTenantId = theId2.ToString();
+            float tAmount = 12.50f;
+
+            string currentTenantIdTest = tTenantId;
+
+            //ACT
+            var mySer = new PaymentService(_Tdb);
+            //var isCheck = mySer.AddTransaction(tReference, tPayId, tTenantId, tAmount);
+            var myTrns = mySer.GetTransactions(currentTenantIdTest);
+            var icount = myTrns.Count();
+            
+
+            //ASSERT
+            Assert.Equal(1, icount);
+        }
+        
         /*Dictionary<string, string> ToDictionary(string response);
         string GetMd5Hash(Dictionary<string, string> data, string encryptionKey);
         bool VerifyMd5Hash(Dictionary<string, string> data, string encryptionKey, string hash);
         public bool AddTransaction(string reference, string payId, string tenantId, float amount);
-        IEnumerable<Transactions> GetTransactions(string currentTenantId);
+        
         public Transactions GetPaidSubscription(string tenantId);
         public void UpdateSubscription(string subscription, string tenantId);
         
