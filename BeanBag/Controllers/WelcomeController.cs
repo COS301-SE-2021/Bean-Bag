d3c572a93bcb2857f6060047be9cf774a1496739
SuzelAlberts <73718151+SuzelAlberts@users.noreply.github.com>
2021-09-16 16:16:59 +0200

    Added WelcomeController.cs with code verification and user sign up

--- BeanBag/Controllers/WelcomeController.cs
+++ BeanBag/Controllers/WelcomeController.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using BeanBag.Services;
+using Microsoft.Identity.Web;
 
 namespace BeanBag.Controllers
 {
-    public class UserSignUpController : Controller
+    public class WelcomeController : Controller
     {
 
         private readonly ITenantService _tenantService;
-        public UserSignUpController(ITenantService tenantService)
+        public WelcomeController(ITenantService tenantService)
         {
             _tenantService = tenantService;
         }
@@ -31,6 +33,19 @@ namespace BeanBag.Controllers
             //Verify the entered code
             if (_tenantService.VerifyCode(code))
             {
+                //Code is verified, sign user up under tenant 
+                var tenant = _tenantService.GetInvitationTenant(code);
+
+                if (tenant == null)
+                {
+                    return BadRequest();
+                }
+
+                if (_tenantService.SignUserUp(User.GetObjectId(), tenant.TenantId, User.GetDisplayName()))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
                 return RedirectToAction("Index");
             }
 
